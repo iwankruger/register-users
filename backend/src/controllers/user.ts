537ebe1e2f82d7c2b1ce5099dc255ed261b07fae
iwan kruger <iwankruger@gmail.com>
2020-09-20 14:53:00 +0200

    Added linter

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,15 +1,21 @@
 import { Router, NextFunction, Request, Response } from 'express';
 
+interface RequestWithBody extends Request {
+    body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     return res.send('hello users');
 });
 
-router.post('/', (req: Request, res: Response, next: NextFunction) => {
+router.post('/', (req: RequestWithBody, res: Response, next: NextFunction) => {
 
     const { name, surname, email } = req.body;
-    console.log('body ', name);
+    if (name) {
+        console.log('body ', name.toUpperCase());
+    }
     return res.send({ result: true });
 });
 
