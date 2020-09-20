80e2d440192eb313e5da3cec2220f65f9811d82f
iwan kruger <iwankruger@gmail.com>
2020-09-20 19:50:18 +0200

    Fixed unknown word warning

--- backend/src/controllers/decorators/controller.ts
+++ backend/src/controllers/decorators/controller.ts
@@ -26,12 +26,12 @@ export function controller(routePrefix: string): any {
             const routeHandler = target.prototype[key];
             const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key);
             const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key);
-            const middlewares = Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) || [];
+            const middlewareArray = Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) || [];
             const requiredBodyProps = Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) || [];
 
             const validator = bodyValidators(requiredBodyProps);
 
-            if (path) router[method](`${routePrefix}${path}`, ...middlewares, validator, routeHandler);
+            if (path) router[method](`${routePrefix}${path}`, ...middlewareArray, validator, routeHandler);
         }
     };
 }
