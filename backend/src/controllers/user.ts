f7bf75eb61d456c7c0b6434de3ac6e1c242df734
iwan kruger <iwankruger@gmail.com>
2020-09-20 14:07:12 +0200

    Added a user route

---
+++ backend/src/controllers/user.ts
@@ -0,0 +1,9 @@
+import { Router, NextFunction, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+    return res.send('hello users');
+});
+
+export = router;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,12 @@ import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
 
+import user from './controllers/user';
+
 app.get('/', (eq: Request, res: Response) => {
     res.send(`<div>hell world!!!</div>`)
 });
+
+app.use('/api/users', user);
 
 app.listen(5000, () => console.log(`Version 1.0.1 App listening on localhost:5000`));
