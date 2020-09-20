759bebe4edeefc02f56cc03d20ad4401ec99de59
iwan kruger <iwankruger@gmail.com>
2020-09-20 13:57:48 +0200

    Added express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello world!!!');
+import * as bodyParser from 'body-parser';
+import express, { NextFunction, Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (eq: Request, res: Response) => {
+    res.send(`<div>hell world!!!</div>`)
+});
+
+app.listen(5000, () => console.log(`Version 1.0.1 App listening on localhost:5000`));
